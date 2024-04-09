ffba56e80f857b21523949933c75af5dc70860d7
kanishkkapoor15 <71957724+kanishkkapoor15@users.noreply.github.com>
2024-04-09 13:02:16 +0530

    Added custom orders

--- myapp/src/Pages/MyOrders.jsx
+++ myapp/src/Pages/MyOrders.jsx
@@ -5,7 +5,10 @@ import './CSS/MyOrders.css'
 
 const MyOrders = () => {
     const [orders, setOrders] = useState([]);
-    const { all_product } = useContext(ShopContext); // Use ShopContext to access all_product
+    const { all_product } = useContext(ShopContext); // Use ShopContext to access all_product'
+    const [customOrders, setCustomOrders] = useState([]);
+
+ 
 
     useEffect(() => {
         const fetchOrders = async () => {
@@ -21,13 +24,34 @@ const MyOrders = () => {
                         'auth-token': token
                     }
                 });
+
                 setOrders(response.data);
+
+               
             } catch (error) {
                 console.error('Error fetching user orders:', error);
             }
+            
         };
 
         fetchOrders();
+
+        const fetchOrders2 = async () => {
+            try {
+              const response = await fetch('http://localhost:4000/all-orders');
+              if (!response.ok) {
+                throw new Error('Failed to fetch orders');
+              }
+              const data = await response.json();
+              setCustomOrders(data.customOrders);
+              console.log(customOrders);
+            } catch (error) {
+              console.error('Error fetching orders:', error);
+              
+            }
+          };
+          fetchOrders2();
+
     }, []);
 
     return (
@@ -64,8 +88,26 @@ const MyOrders = () => {
                       } else {
                           return null;
                       }
-                  })}                  
+                  })}   
 
+
+                    {customOrders.map((order, index) => (
+            <div key={index} className="MyOrders-custom">
+            {console.log("Modified productImage URL:", order.productImage.replace(/\\/g, '/').replace('upload/custom/', ''))}
+            <img src={`http://localhost:4000/${order.productImage.replace(/\\/g, '/').replace('upload/custom/', '')}`} alt="" />
+            <p><b>Product : </b>{order.productType}</p>
+            <p><b>Quantity:</b> {order.productQuantity}{" "}<b>Size:</b>{order.productSize}</p>
+            <p><b>Price:</b> {order.priceOffered}</p>
+            <p><b>Name: </b>{order.userName}</p>
+            <p><b>Order ID:</b> {order._id}</p>
+
+            
+        </div>
+        
+        
+        
+                      ))
+                    }
                 </div>
             )}
         </div>
